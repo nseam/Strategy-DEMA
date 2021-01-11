9af99cd8ac62dbe552ab38b8a218ad266157704b
kenorb <kenorb@users.noreply.github.com>
2021-01-11 03:24:47 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,9 +6,9 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_DEMA_Params_M5 : Indi_DEMA_Params {
   Indi_DEMA_Params_M5() : Indi_DEMA_Params(indi_dema_defaults, PERIOD_M5) {
-    period = 12;
-    ma_shift = 0;
     applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_shift = 0;
+    period = 12;
     shift = 0;
   }
 } indi_dema_m5;
@@ -18,14 +18,14 @@ struct Stg_DEMA_Params_M5 : StgParams {
   // Struct constructor.
   Stg_DEMA_Params_M5() : StgParams(stg_dema_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)10;
-    signal_open_boost = 0;
+    signal_open_method = -4;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)10;
+    signal_close_level = (float)10.0;
     price_stop_method = 0;
-    price_stop_level = 2;
+    price_stop_level = 10.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
